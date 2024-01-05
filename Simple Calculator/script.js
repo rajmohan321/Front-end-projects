0d67db31665aefb9250fb4869803f6a9dfd4994c
MOHANRAJ A <113257663+rajmohan321@users.noreply.github.com>
2024-01-05 12:05:44 +0530

    Update script.js

--- Simple Calculator/script.js
+++ Simple Calculator/script.js
@@ -1,15 +1,12 @@
-// let get display id input tag
+
 let displayNum = document.getElementById("displayNum");
-// we have 3 three function to implement
 function appendToDisplay(input) {
   displayNum.value += input;
 }
 function clearDisplay() {
-  //this is for clearing value in the input
   displayNum.value = "";
 }
 function calculate() {
-  // this is for calculate after we give a input
   try {
     displayNum.value = eval(displayNum.value);
   } catch {
