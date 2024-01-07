0bc9dcec66ff7f887fadc0c422d72d9cf6cf4ee1
MOHANRAJ A <113257663+rajmohan321@users.noreply.github.com>
2024-01-07 21:04:37 +0530

    Update __script.js
    Remove Comments

--- Digital Clock/__script.js
+++ Digital Clock/__script.js
@@ -12,16 +12,6 @@ function showTime() {
     h = h - 12;
     session = "PM";
   }
-  //   if (h < 10) {
-  //     h = "0" + h;
-  //   }
-  //   if (m < 10) {
-  //     m = "0" + m;
-  //   }
-  //   if (s < 10) {
-  //     s = "0" + s;
-  //   }
-  // or using ternary operator
   h = h < 10 ? "0" + h : h;
   m = m < 10 ? "0" + m : m;
   s = s < 10 ? "0" + s : s;
